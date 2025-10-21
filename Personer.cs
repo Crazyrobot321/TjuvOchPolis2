7e6af414b351e8ae153ec199ddea8bcb3553e721
Erik Niva <erikmz40@gmail.com>
2025-10-21 10:31:49 +0200

    Added Personer.cs and function to print city

---
+++ Personer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TjuvOchPolis
+{
+    public class Personer
+    {
+        public int LocationX { get; set; }
+        public int LocationY { get; set; }
+        public int DirectionX { get; set; }
+        public int DirectionY { get; set; }
+        public List<String> Properties{ get; set; }
+
+        public Personer(int locationX, int locationY, int directionX, int directionY, List<String>properties)
+        {
+            LocationX = locationX;
+            LocationY = locationY;
+            DirectionX = directionX;
+            DirectionY = directionY;
+            Properties = properties;
+        }
+    }
+
+    class Citizen : Personer
+    {
+        public Citizen(int locationX, int locationY,int directionX,int directionY, List<String> properties) : base(locationX,locationY,directionX,directionY,properties)
+        {
+            
+        }
+    }
+
+    class Thief : Personer
+    {
+        public bool HasStolen { get; set; }
+        public Thief(int locationX, int locationY, int directionX, int directionY, List<String> properties, bool hasStolen) : base(locationX, locationY, directionX, directionY, properties)
+        {
+            HasStolen = hasStolen;
+        }
+    }
+
+    class Police : Personer
+    {
+        public int CaughtThieves { get; set; }
+        public Police(int locationX, int locationY, int directionX, int directionY, List<String> properties, int caughtThieves) : base(locationX, locationY, directionX, directionY, properties)
+        {
+            CaughtThieves = caughtThieves;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,10 +2,59 @@
 {
     internal class Program
     {
+        public static int height = 25;
+        public static int width = 100;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<String> properties = new List<String> { "Keys", "Mobile", "Wallet", "Watch" };
+            List<String> seizedGoods = new List<String>();
+            List<String> StolenItems = new List<String>();
+            List<Personer> personer = new List<Personer>();
 
+            for(int i = 0; i < 20; i++)
+            {
+                personer.Add(new Citizen(Random.Shared.Next(width), Random.Shared.Next(height), Random.Shared.Next(-1, 2), Random.Shared.Next(-1, 2), properties));
+            }
+            while (true)
+            {
+                RenderGameBoard();
+                Thread.Sleep(500);
+                Console.ReadKey();
+            }
+
+        }
+
+        private static void RenderGameBoard()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (y == 0 || y == height - 1)
+                    {
+                        if (y == 0 && x == 5)
+                        {
+                            Console.Write(" CITY ");
+                            x += " CITY ".Length - 1;
+                        }
+                        else
+                        {
+                            Console.Write("=");
+                        }
+                    }
+
+                    else if (x == 0 || x == width - 1)
+                    {
+                        Console.Write("||");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
